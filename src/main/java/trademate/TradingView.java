ecc8c69687b2b738317065c0f1f2fe6fbeb4c2c6
Teletha <Teletha.T@gmail.com>
2018-01-05 12:31:23 +0900

    FIX bug.

--- src/main/java/trademate/TradingView.java
+++ src/main/java/trademate/TradingView.java
@@ -101,6 +101,7 @@ public class TradingView extends View {
             long minute = 60000;
             axis.tickLabelFormatter
                     .set(v -> Instant.ofEpochMilli((long) v).plus(9, ChronoUnit.HOURS).atZone(Execution.UTC).format(formatter));
+            axis.visibleRange.set(150D / serise.ticks.size());
             axis.visualMinValue.set(System.currentTimeMillis() - 10 * 60 * 1000);
             axis.visualMaxValue.set(System.currentTimeMillis() + 10 * 60 * 1000);
             axis.units.set(new double[] {minute, 5 * minute, 10 * minute, 30 * minute, 60 * minute, 2 * 60 * minute, 4 * 60 * minute,
