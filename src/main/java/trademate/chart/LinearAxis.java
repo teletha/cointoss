30168e9eec047fca357a3c6b61f7d53c77aa2716
Teletha <Teletha.T@gmail.com>
2018-01-05 11:44:25 +0900

    Refactoring axis label.

--- src/main/java/trademate/chart/LinearAxis.java
+++ src/main/java/trademate/chart/LinearAxis.java
@@ -29,9 +29,12 @@ public class LinearAxis extends Axis {
      * We use these for auto ranging to pick a user friendly tick unit. We handle tick units in the
      * range of 1e-10 to 1e+12
      */
-    private static final double[] TickUnit = {0.0010d, 0.0025d, 0.0050d, 0.01d, 0.025d, 0.05d, 0.1d, 0.25d, 0.5d, 1.0d, 2.5d, 5.0d, 10.0d,
-            25.0d, 50.0d, 100.0d, 250.0d, 500.0d, 1000.0d, 2500.0d, 5000.0d, 10000.0d, 25000.0d, 50000.0d, 100000.0d, 250000.0d, 500000.0d,
-            1000000.0d, 2500000.0d, 5000000.0d};
+    private static final double[] DefaultTickUnit = {1.0E-10d, 2.5E-10d, 5.0E-10d, 1.0E-9d, 2.5E-9d, 5.0E-9d, 1.0E-8d, 2.5E-8d, 5.0E-8d,
+            1.0E-7d, 2.5E-7d, 5.0E-7d, 1.0E-6d, 2.5E-6d, 5.0E-6d, 1.0E-5d, 2.5E-5d, 5.0E-5d, 1.0E-4d, 2.5E-4d, 5.0E-4d, 0.0010d, 0.0025d,
+            0.0050d, 0.01d, 0.025d, 0.05d, 0.1d, 0.25d, 0.5d, 1.0d, 2.5d, 5.0d, 10.0d, 25.0d, 50.0d, 100.0d, 250.0d, 500.0d, 1000.0d,
+            2500.0d, 5000.0d, 10000.0d, 25000.0d, 50000.0d, 100000.0d, 250000.0d, 500000.0d, 1000000.0d, 2500000.0d, 5000000.0d, 1.0E7d,
+            2.5E7d, 5.0E7d, 1.0E8d, 2.5E8d, 5.0E8d, 1.0E9d, 2.5E9d, 5.0E9d, 1.0E10d, 2.5E10d, 5.0E10d, 1.0E11d, 2.5E11d, 5.0E11d, 1.0E12d,
+            2.5E12d, 5.0E12d};
 
     public final ObjectProperty<DoubleToObjectFunction<String>> tickLabelFormatter = new SimpleObjectProperty<>(this, "tickLabelFormatter", String::valueOf);
 
@@ -39,7 +42,10 @@ public class LinearAxis extends Axis {
 
     private double uiRatio;
 
-    public final ObjectProperty<double[]> units = new SimpleObjectProperty(this, "units", TickUnit);
+    /** The current unit index. */
+    private int currentUnitIndex = -1;
+
+    public final ObjectProperty<double[]> units = new SimpleObjectProperty(DefaultTickUnit);
 
     /**
      * @param tickLength
@@ -68,8 +74,6 @@ public class LinearAxis extends Axis {
         }
         return position / uiRatio + lowVal;
     }
-
-    private int unitIndex = -1;
 
     /**
      * {@inheritDoc}
@@ -140,10 +144,8 @@ public class LinearAxis extends Axis {
         // search sutable unit
         double visibleValueDistance = up - low;
         int nextUnitIndex = findNearestUnitIndex(visibleValueDistance / tickNumber.get());
-        boolean usePrevious = nextUnitIndex == unitIndex;
 
         double nextUnitSize = units.get()[nextUnitIndex];
-
         double visibleStartUnitBasedValue = floor(low / nextUnitSize) * nextUnitSize;
         double uiRatio = axisLength / visibleValueDistance;
 
@@ -158,8 +160,9 @@ public class LinearAxis extends Axis {
         boolean isH = isHorizontal();
 
         ObservableList<AxisLabel> labels = getLabels();
-        if (!usePrevious) {
+        if (currentUnitIndex != nextUnitIndex) {
             labels.clear();
+            currentUnitIndex = nextUnitIndex;
         }
 
         ArrayList<AxisLabel> unused = new ArrayList<>(labels);
