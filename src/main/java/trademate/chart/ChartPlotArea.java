4da25807a71ef1140c9b57a48538e46ea7a21878
Teletha <Teletha.T@gmail.com>
2018-01-09 11:12:33 +0900

    Fix back grid overflow.

--- src/main/java/trademate/chart/ChartPlotArea.java
+++ src/main/java/trademate/chart/ChartPlotArea.java
@@ -121,6 +121,8 @@ public class ChartPlotArea extends Region {
         axisY.scroll.visibleAmountProperty().addListener(plotValidateListener);
 
         Viewtify.clip(this);
+        Viewtify.clip(backGridVertical);
+        Viewtify.clip(backGridHorizontal);
 
         visualizeMouseTrack();
         visualizeNotifyPrice();
