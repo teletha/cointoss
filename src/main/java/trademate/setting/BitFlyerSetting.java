f0f34749c516e549c935da10ec6e3f9520905a0a
Teletha <Teletha.T@gmail.com>
2018-08-29 11:09:11 +0900

    Progress...

--- src/main/java/trademate/setting/BitFlyerSetting.java
+++ src/main/java/trademate/setting/BitFlyerSetting.java
@@ -9,6 +9,7 @@
  */
 package trademate.setting;
 
+import kiss.Extensible;
 import kiss.Manageable;
 import kiss.Singleton;
 import viewtify.View;
@@ -19,11 +20,45 @@ import viewtify.View;
 @Manageable(lifestyle = Singleton.class)
 public class BitFlyerSetting extends View {
 
+    /** The message resource. */
+    private final Lang $ = localizeBy(Lang.class);
+
     /**
      * {@inheritDoc}
      */
     @Override
     protected void initialize() {
         System.out.println("Market View");
+    }
+
+    /**
+     * @version 2018/08/29 3:52:37
+     */
+    @SuppressWarnings("unused")
+    @Manageable(lifestyle = Singleton.class)
+    private static class Lang implements Extensible {
+
+        /**
+         * Label for API key
+         * 
+         * @return
+         */
+        String publicAPIDescription() {
+            return "Please get API key and API secret to use the public API provided by [BitFlyer](https://lightning.bitflyer.jp/developer).";
+        }
+
+        /**
+         * @version 2018/08/29 3:53:49
+         */
+        private static class Lang_ja extends Lang {
+
+            /**
+             * {@inheritDoc}
+             */
+            @Override
+            String publicAPIDescription() {
+                return "[BitFlyer](https://lightning.bitflyer.jp/developer)の提供する公開APIを利用するためにAPIキーとAPIシークレットを取得してください。";
+            }
+        }
     }
 }
