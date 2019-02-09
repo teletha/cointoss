b9efc8ade910294cf9e2ebd0cda76a7ac04fa980
Teletha <Teletha.T@gmail.com>
2019-02-10 02:43:21 +0900

    Update psychopath.

--- src/project/java/Project.java
+++ src/project/java/Project.java
@@ -17,7 +17,6 @@ public class Project extends bee.api.Project {
         require("com.github.teletha", "sinobu", "1.0");
         require("com.github.teletha", "viewtify", "1.0");
         require("com.github.teletha", "marionette", "0.2");
-        require("com.github.teletha", "stylist", "0.1");
         require("com.github.teletha", "antibug", "0.6").atTest();
         require("org.apache.commons", "commons-lang3", "3.7");
         require("commons-codec", "commons-codec", "1.11");
