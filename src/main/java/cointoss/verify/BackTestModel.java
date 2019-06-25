7cc9dd1dcc2efe382cfadd6788ff59eec5a1a38e
Teletha <Teletha.T@gmail.com>
2019-06-25 12:01:19 +0900

    Provide back tester.

---
+++ src/main/java/cointoss/verify/BackTestModel.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2019 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package cointoss.verify;
+
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.function.Function;
+
+import cointoss.Market;
+import cointoss.MarketService;
+import cointoss.trade.Trader;
+import cointoss.trade.TradingLog;
+import cointoss.util.Chrono;
+import icy.manipulator.Icy;
+import kiss.I;
+
+@Icy
+public interface BackTestModel {
+
+    @Icy.Property
+    MarketService service();
+
+    @Icy.Property
+    ZonedDateTime start();
+
+    @Icy.Overload("start")
+    private ZonedDateTime start(int year, int month, int day) {
+        return Chrono.utc(LocalDate.of(year, month, day));
+    }
+
+    @Icy.Property
+    ZonedDateTime end();
+
+    @Icy.Overload("end")
+    private ZonedDateTime end(int year, int month, int day) {
+        return Chrono.utc(LocalDate.of(year, month, day));
+    }
+
+    default List<TradingLog> run(Function<Market, Trader> traderBuilder) {
+        return runs(market -> List.of(traderBuilder.apply(market)));
+    }
+
+    default List<TradingLog> runs(Function<Market, List<Trader>> traderBuilder) {
+        VerifiableMarket market = new VerifiableMarket(service());
+
+        List<Trader> traders = traderBuilder.apply(market);
+
+        market.readLog(log -> log.range(start(), end()).effect(e -> market.perform(e)));
+
+        return I.signal(traders).map(Trader::log).toList();
+    }
+}
---
+++ src/test/java/cointoss/verify/BackTestInvoker.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2019 CoinToss Development Team
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          https://opensource.org/licenses/MIT
+ */
+package cointoss.verify;
+
+import static cointoss.ticker.TickSpan.*;
+
+import cointoss.Direction;
+import cointoss.Market;
+import cointoss.market.bitflyer.BitFlyer;
+import cointoss.trade.Trader;
+import cointoss.trade.TradingLog;
+
+public class BackTestInvoker {
+
+    public static void main(String[] args) {
+        TradingLog log = BackTest.with.service(BitFlyer.FX_BTC_JPY)
+                .start(2019, 6, 1)
+                .end(2019, 6, 1)
+                .run(market -> new Sample(market))
+                .get(0);
+
+        System.out.println(log);
+    }
+
+    /**
+     * 
+     */
+    private static class Sample extends Trader {
+
+        private Sample(Market market) {
+            super(market);
+
+            when(market.tickers.of(Hour1).add, tick -> {
+                return new Entry(Direction.random()) {
+
+                    @Override
+                    protected void order() {
+                        order(1, s -> s.make(market.tickers.latest.v.price));
+                    }
+
+                    /**
+                     * {@inheritDoc}
+                     */
+                    @Override
+                    protected void exit() {
+                        exitAt(entryPrice.plus(direction, 3000));
+                    }
+                };
+            });
+        }
+    }
+}
