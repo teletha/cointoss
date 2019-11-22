c66abbbb91a9dd64284b47654085825542e10e96
Teletha <Teletha.T@gmail.com>
2019-11-22 23:52:49 +0900

    Remove unused code.

--- src/test/java/cointoss/verify/BackTestInvoker.java
+++ src/test/java/cointoss/verify/BackTestInvoker.java
@@ -9,18 +9,8 @@
  */
 package cointoss.verify;
 
-import static java.time.temporal.ChronoUnit.MINUTES;
-
-import cointoss.Direction;
-import cointoss.Market;
-import cointoss.Scenario;
-import cointoss.Trader;
 import cointoss.market.bitflyer.BitFlyer;
-import cointoss.ticker.Indicator;
-import cointoss.ticker.Span;
 import cointoss.trading.LazyBear;
-import cointoss.util.Num;
-import kiss.I;
 
 public class BackTestInvoker {
 
@@ -32,38 +22,5 @@ public class BackTestInvoker {
                 .initialBaseCurrency(3000000)
                 .detail(true)
                 .run();
-    }
-
-    /**
-     * 
-     */
-    private static class Sample extends Trader {
-
-        Indicator<Num> losscutRange = Indicator.averageTrueRange(market.tickers.of(Span.Minute30), 6);
-
-        private Sample(Market market) {
-            super(market);
-
-            when(market.tickers.of(Span.Minute5).add.skip(12), tick -> new Scenario() {
-
-                @Override
-                protected void entry() {
-                    entry(Direction.random(), 0.1, s -> s.make(market.tickers.latestPrice.v).cancelAfter(3, MINUTES));
-                }
-
-                @Override
-                protected void exit() {
-                    exitAt(entryPrice.plus(this, 6400));
-                    exitAt(market.tickers.of(Span.Second5).add.flatMap(tick -> {
-                        if (tick.openPrice.isGreaterThan(this, entryPrice.plus(this, 2800))) {
-                            return I.signal(entryPrice.plus(this, 500));
-                        } else {
-                            return I.signal();
-                        }
-                    }).first().startWith(entryPrice.minus(this, losscutRange.last())).to());
-                    // exitAt(trailing2(up -> entryPrice.minus(this, 1300).plus(this, up)));
-                }
-            });
-        }
     }
 }
