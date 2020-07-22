adba5ed6972f366286991427a0af27b5f20e0b7b
Teletha <Teletha.T@gmail.com>
2020-07-23 01:50:34 +0900

    Test execution api on bitmex.

--- src/test/java/cointoss/market/bitmex/BitmexPublicServiceTest.java
+++ src/test/java/cointoss/market/bitmex/BitmexPublicServiceTest.java
@@ -87,7 +87,30 @@ class BitmexPublicServiceTest extends PublicServiceTestTemplate<BitMexService> {
     @Override
     @Test
     public void executionLatest() {
-        super.executionLatest();
+        httpClient.onGet().doReturn("""
+                [
+                  {
+                    "timestamp": "2020-07-22T16:34:27.324Z",
+                    "symbol": "XBTUSD",
+                    "side": "Buy",
+                    "size": 1,
+                    "price": 9344.5,
+                    "tickDirection": "ZeroPlusTick",
+                    "trdMatchID": "06cde094-96ba-9878-2f8b-534aef8b1270",
+                    "grossValue": 10701,
+                    "homeNotional": 0.00010701,
+                    "foreignNotional": 1
+                  }
+                ]
+                """);
+
+        Execution e = service.executionLatest().to().exact();
+        assert e.id == 159543566732400000L;
+        assert e.direction == Direction.BUY;
+        assert e.price.is(9344.5);
+        assert e.size.is(0.00010701);
+        assert e.date.isEqual(Chrono.utc(2020, 7, 22, 16, 34, 27, 324));
+        assert e.consecutive == Execution.ConsecutiveDifference;
     }
 
     /**
@@ -96,7 +119,40 @@ class BitmexPublicServiceTest extends PublicServiceTestTemplate<BitMexService> {
     @Override
     @Test
     public void executionRealtimely() {
-        super.executionRealtimely();
+        websocketServer.replyWhenJSON("{'args':['trade:XBTUSD'],'op':'subscribe'}", server -> {
+            server.sendJSON("{'info':'Welcome to the BitMEX Realtime API.','version':'2020-07-14T21:30:54.000Z','timestamp':'2020-07-22T16:36:44.022Z','docs':'https://www.bitmex.com/app/wsAPI','limit':{'remaining':39}}");
+            server.sendJSON("{'success':true,'subscribe':'trade:XBTUSD','request':{'args':['trade:XBTUSD'],'op':'subscribe'}}");
+            server.sendJSON("{'table':'trade','action':'partial','keys':[],'types':{'timestamp':'timestamp','symbol':'symbol','side':'symbol','size':'long','price':'float','tickDirection':'symbol','trdMatchID':'guid','grossValue':'long','homeNotional':'float','foreignNotional':'float'},'foreignKeys':{'symbol':'instrument','side':'side'},'attributes':{'timestamp':'sorted','symbol':'grouped'},'filter':{'symbol':'XBTUSD'},'data':[{'timestamp':'2020-07-22T16:36:43.440Z','symbol':'XBTUSD','side':'Buy','size':102,'price':9344.5,'tickDirection':'PlusTick','trdMatchID':'e482b0d5-5749-9cc5-23d9-864514816f53','grossValue':1091502,'homeNotional':0.01091502,'foreignNotional':102}]}");
+            server.sendJSON("{'table':'trade','action':'insert','data':[{'timestamp':'2020-07-22T16:36:48.264Z','symbol':'XBTUSD','side':'Buy','size':5000,'price':9344.5,'tickDirection':'ZeroPlusTick','trdMatchID':'0890e57d-366f-0394-6fac-3fac3b5b5569','grossValue':53505000,'homeNotional':0.53505,'foreignNotional':5000}]}");
+            server.sendJSON("{'table':'trade','action':'insert','data':[{'timestamp':'2020-07-22T16:36:53.792Z','symbol':'XBTUSD','side':'Sell','size':1,'price':9344,'tickDirection':'MinusTick','trdMatchID':'f92979ae-b84a-8e0e-07b5-aa3ab590f0b7','grossValue':10702,'homeNotional':0.00010702,'foreignNotional':1}]}");
+        });
+
+        List<Execution> list = service.executionsRealtimely().toList();
+        assert list.size() == 3;
+
+        Execution exe = list.get(0);
+        assert exe.id == 159543580344000000L;
+        assert exe.direction == Direction.BUY;
+        assert exe.price.is(9344.5);
+        assert exe.size.is(0.01091502);
+        assert exe.date.isEqual(Chrono.utc(2020, 7, 22, 16, 36, 43, 440));
+        assert exe.consecutive == Execution.ConsecutiveDifference;
+
+        exe = list.get(1);
+        assert exe.id == 159543580826400000L;
+        assert exe.direction == Direction.BUY;
+        assert exe.price.is(9344.5);
+        assert exe.size.is(0.53505);
+        assert exe.date.isEqual(Chrono.utc(2020, 7, 22, 16, 36, 48, 264));
+        assert exe.consecutive == Execution.ConsecutiveDifference;
+
+        exe = list.get(2);
+        assert exe.id == 159543581379200000L;
+        assert exe.direction == Direction.SELL;
+        assert exe.price.is(9344);
+        assert exe.size.is(0.00010702);
+        assert exe.date.isEqual(Chrono.utc(2020, 7, 22, 16, 36, 53, 792));
+        assert exe.consecutive == Execution.ConsecutiveDifference;
     }
 
     /**
@@ -105,7 +161,17 @@ class BitmexPublicServiceTest extends PublicServiceTestTemplate<BitMexService> {
     @Override
     @Test
     public void executionRealtimelyConsecutiveBuy() {
-        super.executionRealtimelyConsecutiveBuy();
+        websocketServer.replyWhenJSON("{'args':['trade:XBTUSD'],'op':'subscribe'}", server -> {
+            server.sendJSON("{'info':'Welcome to the BitMEX Realtime API.','version':'2020-07-14T21:30:54.000Z','timestamp':'2020-07-22T16:36:44.022Z','docs':'https://www.bitmex.com/app/wsAPI','limit':{'remaining':39}}");
+            server.sendJSON("{'success':true,'subscribe':'trade:XBTUSD','request':{'args':['trade:XBTUSD'],'op':'subscribe'}}");
+            server.sendJSON("{'table':'trade','action':'partial','keys':[],'types':{'timestamp':'timestamp','symbol':'symbol','side':'symbol','size':'long','price':'float','tickDirection':'symbol','trdMatchID':'guid','grossValue':'long','homeNotional':'float','foreignNotional':'float'},'foreignKeys':{'symbol':'instrument','side':'side'},'attributes':{'timestamp':'sorted','symbol':'grouped'},'filter':{'symbol':'XBTUSD'},'data':[{'timestamp':'2020-07-22T16:36:43.440Z','symbol':'XBTUSD','side':'Buy','size':102,'price':9344.5,'tickDirection':'PlusTick','trdMatchID':'e482b0d5-5749-9cc5-23d9-864514816f53','grossValue':1091502,'homeNotional':0.01091502,'foreignNotional':102}]}");
+            server.sendJSON("{'table':'trade','action':'insert','data':[{'timestamp':'2020-07-22T16:36:43.440Z','symbol':'XBTUSD','side':'Buy','size':5000,'price':9344.5,'tickDirection':'ZeroPlusTick','trdMatchID':'0890e57d-366f-0394-6fac-3fac3b5b5569','grossValue':53505000,'homeNotional':0.53505,'foreignNotional':5000}]}");
+        });
+
+        List<Execution> list = service.executionsRealtimely().toList();
+        assert list.size() == 2;
+        assert list.get(0).consecutive == Execution.ConsecutiveDifference;
+        assert list.get(1).consecutive == Execution.ConsecutiveSameBuyer;
     }
 
     /**
@@ -114,7 +180,17 @@ class BitmexPublicServiceTest extends PublicServiceTestTemplate<BitMexService> {
     @Override
     @Test
     public void executionRealtimelyConsecutiveSell() {
-        super.executionRealtimelyConsecutiveSell();
+        websocketServer.replyWhenJSON("{'args':['trade:XBTUSD'],'op':'subscribe'}", server -> {
+            server.sendJSON("{'info':'Welcome to the BitMEX Realtime API.','version':'2020-07-14T21:30:54.000Z','timestamp':'2020-07-22T16:36:44.022Z','docs':'https://www.bitmex.com/app/wsAPI','limit':{'remaining':39}}");
+            server.sendJSON("{'success':true,'subscribe':'trade:XBTUSD','request':{'args':['trade:XBTUSD'],'op':'subscribe'}}");
+            server.sendJSON("{'table':'trade','action':'partial','keys':[],'types':{'timestamp':'timestamp','symbol':'symbol','side':'symbol','size':'long','price':'float','tickDirection':'symbol','trdMatchID':'guid','grossValue':'long','homeNotional':'float','foreignNotional':'float'},'foreignKeys':{'symbol':'instrument','side':'side'},'attributes':{'timestamp':'sorted','symbol':'grouped'},'filter':{'symbol':'XBTUSD'},'data':[{'timestamp':'2020-07-22T16:36:43.440Z','symbol':'XBTUSD','side':'Sell','size':102,'price':9344.5,'tickDirection':'PlusTick','trdMatchID':'e482b0d5-5749-9cc5-23d9-864514816f53','grossValue':1091502,'homeNotional':0.01091502,'foreignNotional':102}]}");
+            server.sendJSON("{'table':'trade','action':'insert','data':[{'timestamp':'2020-07-22T16:36:43.440Z','symbol':'XBTUSD','side':'Sell','size':5000,'price':9344.5,'tickDirection':'ZeroPlusTick','trdMatchID':'0890e57d-366f-0394-6fac-3fac3b5b5569','grossValue':53505000,'homeNotional':0.53505,'foreignNotional':5000}]}");
+        });
+
+        List<Execution> list = service.executionsRealtimely().toList();
+        assert list.size() == 2;
+        assert list.get(0).consecutive == Execution.ConsecutiveDifference;
+        assert list.get(1).consecutive == Execution.ConsecutiveSameSeller;
     }
 
     /**
@@ -123,6 +199,22 @@ class BitmexPublicServiceTest extends PublicServiceTestTemplate<BitMexService> {
     @Override
     @Test
     public void executionRealtimelyWithMultipleChannels() {
-        super.executionRealtimelyWithMultipleChannels();
+        websocketServer.replyWhenJSON("{'args':['trade:XBTUSD'],'op':'subscribe'}", server -> {
+            server.sendJSON("{'info':'Welcome to the BitMEX Realtime API.','version':'2020-07-14T21:30:54.000Z','timestamp':'2020-07-22T16:36:44.022Z','docs':'https://www.bitmex.com/app/wsAPI','limit':{'remaining':39}}");
+            server.sendJSON("{'success':true,'subscribe':'trade:XBTUSD','request':{'args':['trade:XBTUSD'],'op':'subscribe'}}");
+            server.sendJSON("{'table':'trade','action':'partial','keys':[],'types':{'timestamp':'timestamp','symbol':'symbol','side':'symbol','size':'long','price':'float','tickDirection':'symbol','trdMatchID':'guid','grossValue':'long','homeNotional':'float','foreignNotional':'float'},'foreignKeys':{'symbol':'instrument','side':'side'},'attributes':{'timestamp':'sorted','symbol':'grouped'},'filter':{'symbol':'XBTUSD'},'data':[{'timestamp':'2020-07-22T16:36:43.440Z','symbol':'XBTUSD','side':'Sell','size':102,'price':9344.5,'tickDirection':'PlusTick','trdMatchID':'e482b0d5-5749-9cc5-23d9-864514816f53','grossValue':1091502,'homeNotional':0.01091502,'foreignNotional':102}]}");
+            server.sendJSON("{'table':'trade','action':'insert','data':[{'timestamp':'2020-07-22T16:36:43.440Z','symbol':'ETHUSD','side':'Sell','size':5000,'price':9344.5,'tickDirection':'ZeroPlusTick','trdMatchID':'0890e57d-366f-0394-6fac-3fac3b5b5569','grossValue':53505000,'homeNotional':0.53505,'foreignNotional':5000}]}");
+        });
+
+        List<Execution> list = service.executionsRealtimely().toList();
+        assert list.size() == 1;
+
+        Execution exe = list.get(0);
+        assert exe.id == 159543580344000000L;
+        assert exe.direction == Direction.SELL;
+        assert exe.price.is(9344.5);
+        assert exe.size.is(0.01091502);
+        assert exe.date.isEqual(Chrono.utc(2020, 7, 22, 16, 36, 43, 440));
+        assert exe.consecutive == Execution.ConsecutiveDifference;
     }
 }
