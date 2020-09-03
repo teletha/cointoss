5d0e7124bf2a5e4b851c8906e67a98e53c292873
Teletha <Teletha.T@gmail.com>
2020-09-03 12:14:31 +0900

    Progress...

--- src/main/java/cointoss/util/decimal/Decimal.java
+++ src/main/java/cointoss/util/decimal/Decimal.java
@@ -9,11 +9,13 @@
  */
 package cointoss.util.decimal;
 
-public class Decimal {
+import cointoss.util.math.BigDecimal;
 
-    private final long v;
+public class Decimal extends Number {
 
-    private final int scale;
+    private long v;
+
+    private int scale;
 
     /**
      * @param value
@@ -28,7 +30,7 @@ public class Decimal {
         if (scale == value.scale) {
             return new Decimal(v + value.v, scale);
         } else if (scale < value.scale) {
-            
+
         }
         return new Decimal(v / value.v, scale - value.scale);
     }
@@ -38,7 +40,171 @@ public class Decimal {
     }
 
     public Decimal divide(Decimal value) {
-        return new Decimal(v / value.v, scale - value.scale);
+        Decimal result = Decimal.of((double) v / value.v);
+        result.scale = scale - value.scale + result.scale;
+        return result;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int intValue() {
+        return (int) (v * pow(-scale));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public long longValue() {
+        return 0;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public float floatValue() {
+        return 0;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public double doubleValue() {
+        return v * pow(-scale);
+    }
+
+    private double pow(int scale) {
+        switch (scale) {
+        case 0:
+            return 1;
+        case 1:
+            return 10;
+        case 2:
+            return 100;
+        case 3:
+            return 1000;
+        case 4:
+            return 10000;
+        case 5:
+            return 100000;
+        case 6:
+            return 1000000;
+        case 7:
+            return 10000000;
+        case 8:
+            return 100000000;
+        case 9:
+            return 1000000000;
+        case 10:
+            return 10000000000d;
+        case 11:
+            return 100000000000d;
+        case 12:
+            return 1000000000000d;
+        case 13:
+            return 10000000000000d;
+        case 14:
+            return 100000000000000d;
+        case 15:
+            return 1000000000000000d;
+        case 16:
+            return 10000000000000000d;
+        case 17:
+            return 100000000000000000d;
+        case 18:
+            return 1000000000000000000d;
+        case 19:
+            return 10000000000000000000d;
+        case 20:
+            return 100000000000000000000d;
+        case 21:
+            return 1000000000000000000000d;
+        case 22:
+            return 10000000000000000000000d;
+        case 23:
+            return 100000000000000000000000d;
+        case 24:
+            return 1000000000000000000000000d;
+        case 25:
+            return 10000000000000000000000000d;
+        case 26:
+            return 100000000000000000000000000d;
+        case 27:
+            return 1000000000000000000000000000d;
+        case 28:
+            return 10000000000000000000000000000d;
+        case 29:
+            return 100000000000000000000000000000d;
+        case 30:
+            return 1000000000000000000000000000000d;
+
+        case -1:
+            return 0.1;
+        case -2:
+            return 0.01;
+        case -3:
+            return 0.001;
+        case -4:
+            return 0.0001;
+        case -5:
+            return 0.00001;
+        case -6:
+            return 0.000001;
+        case -7:
+            return 0.0000001;
+        case -8:
+            return 0.00000001;
+        case -9:
+            return 0.000000001;
+        case -10:
+            return 0.0000000001;
+        case -11:
+            return 0.00000000001;
+        case -12:
+            return 0.000000000001;
+        case -13:
+            return 0.0000000000001;
+        case -14:
+            return 0.00000000000001;
+        case -15:
+            return 0.000000000000001;
+        case -16:
+            return 0.0000000000000001;
+        case -17:
+            return 0.00000000000000001;
+        case -18:
+            return 0.000000000000000001;
+        case -19:
+            return 0.0000000000000000001;
+        case -20:
+            return 0.00000000000000000001;
+        case -21:
+            return 0.000000000000000000001;
+        case -22:
+            return 0.0000000000000000000001;
+        case -23:
+            return 0.00000000000000000000001;
+        case -24:
+            return 0.000000000000000000000001;
+        case -25:
+            return 0.0000000000000000000000001;
+        case -26:
+            return 0.00000000000000000000000001;
+        case -27:
+            return 0.000000000000000000000000001;
+        case -28:
+            return 0.0000000000000000000000000001;
+        case -29:
+            return 0.00000000000000000000000000001;
+        case -30:
+            return 0.000000000000000000000000000001;
+        default:
+            throw new Error("Overflow");
+        }
     }
 
     /**
@@ -83,7 +249,19 @@ public class Decimal {
         return new Decimal((long) (value * Math.pow(10, scale)), scale);
     }
 
+    /**
+     * Construct {@link Decimal} by the specified value.
+     * 
+     * @param value Your value.
+     * @return Immutable {@link Decimal}.
+     */
+    public static Decimal of(String value) {
+        BigDecimal big = new BigDecimal(value);
+        int scale = Math.max(0, big.scale());
+        return new Decimal(big.scaleByPowerOfTen(scale).longValue(), scale);
+    }
+
     static int computeScale(double value) {
-        return 1;
+        return Math.max(0, BigDecimal.valueOf(value).scale());
     }
 }
--- src/test/java/cointoss/util/decimal/DecimalTest.java
+++ src/test/java/cointoss/util/decimal/DecimalTest.java
@@ -26,4 +26,22 @@ class DecimalTest {
         assert Decimal.computeScale(0.02) == 2;
         assert Decimal.computeScale(0.02040) == 4;
     }
+
+    @Test
+    void multiply() {
+        assert Decimal.of(10).multiply(Decimal.of(20)).doubleValue() == 200;
+        assert Decimal.of(10).multiply(Decimal.of(12345678)).doubleValue() == 123456780;
+        assert Decimal.of(10).multiply(Decimal.of(0.1)).doubleValue() == 1;
+        assert Decimal.of(10).multiply(Decimal.of("0.00002")).doubleValue() == 0.0002;
+        assert Decimal.of(10).multiply(Decimal.of(0.1234567)).doubleValue() == 1.234567;
+    }
+
+    @Test
+    void divide() {
+        assert Decimal.of(10).divide(Decimal.of(20)).doubleValue() == 0.5;
+        assert Decimal.of(10).divide(Decimal.of(12345678)).doubleValue() == 8.100000664200054E-7;
+        assert Decimal.of(10).divide(Decimal.of(0.1)).doubleValue() == 100;
+        assert Decimal.of(10).divide(Decimal.of(0.00002)).intValue() == 500000;
+        assert Decimal.of(10).divide(Decimal.of(0.1234567)).doubleValue() == 81.00005913004316;
+    }
 }
