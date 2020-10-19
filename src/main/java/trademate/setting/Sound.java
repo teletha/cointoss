876732079fc1869a31b22bb66ba4a84c014822f5
Teletha <Teletha.T@gmail.com>
2020-10-19 14:42:34 +0900

    Sound notification can configure its volume.

--- src/main/java/trademate/setting/Sound.java
+++ src/main/java/trademate/setting/Sound.java
@@ -13,43 +13,35 @@ import java.util.concurrent.TimeUnit;
 
 import javafx.scene.media.AudioClip;
 
+import kiss.I;
 import kiss.Signaling;
 import viewtify.Viewtify;
 
-/**
- * @version 2018/03/16 20:13:19
- */
 public enum Sound {
 
     Defeat, Dominating, DoubleKill, EnemyDominating, EnemyDoubleKill, EnemyGodlike, EnemyInhibitorIsRespawned, EnemyKillingSpree, EnemyLegendary, EnemyLegendaryKill, EnemyPentaKill, EnemyQuadraKill, EnemyRampage, EnemyTripleKill, EnemyUnstoppable, Godlike, KillingSpree, Legendary, LegendaryKill, MinionHasSpawned, PentaKill, QuadraKill, Rampage, SummonerWasDisconnected, TripleKill, Unstoppable, Victory, WelcomtoSummonersRift, YourTeamDestoryedInhibitor, うわーん, がーん, じゃじゃーん, だよ, ばばーん, やったー, エラー, クリア, ゴール, スタート, ブレークポイント, 上, 下, 何か不安定みたい, 削除, 失敗, 始めるよ, 完了, 成功, 来た, 注目, 行くよ, 追加, なし;
 
-    private final Signaling<String> ons = new Signaling<>();
+    private final Signaling<Double> ons = new Signaling<>();
 
     /**
      * 
      */
     private Sound() {
-        ons.expose.throttle(1000, TimeUnit.MILLISECONDS)
-                .on(Viewtify.UIThread)
-                .map(v -> new AudioClip(ClassLoader.getSystemResource("sound/" + name() + ".m4a").toExternalForm()))
-                .to(AudioClip::play);
+        ons.expose.throttle(1000, TimeUnit.MILLISECONDS).on(Viewtify.UIThread).to(v -> {
+            AudioClip clip = new AudioClip(ClassLoader.getSystemResource("sound/" + name() + ".m4a").toExternalForm());
+            clip.setVolume(v / 100);
+            clip.play();
+        });
     }
 
     /**
      * Play this sound.
      */
     public void play() {
-        if (this != なし) {
-            ons.accept("PLAY");
+        double volume = I.make(Notificator.class).masterVolume.exact();
+
+        if (this != なし && volume != 0) {
+            ons.accept(volume);
         }
     }
-
-    // public static void main(String[] args) {
-    // String value = Locator.directory("src/main/resources/sound")
-    // .walkFile()
-    // .map(file -> file.base())
-    // .scan(Collectors.joining(", ", "", ";"))
-    // .to().v;
-    // System.out.println(value);
-    // }
 }
