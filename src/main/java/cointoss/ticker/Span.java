a3261861485f0e9be25055d7d9b7f318a7a13e42
Teletha <Teletha.T@gmail.com>
2020-12-24 14:22:19 +0900

    Change Span's segment size.

--- src/main/java/cointoss/ticker/Span.java
+++ src/main/java/cointoss/ticker/Span.java
@@ -10,10 +10,12 @@
 package cointoss.ticker;
 
 import static java.time.temporal.ChronoField.*;
+import static java.time.temporal.ChronoUnit.*;
 
 import java.time.Duration;
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
 
 import kiss.I;
 import kiss.Variable;
@@ -23,43 +25,43 @@ import kiss.Variable;
  */
 public enum Span {
 
-    Second5(5, SECOND_OF_MINUTE, 1, HOUR_OF_DAY, 3, 1), // 12 * 60 * 3 = 2160
+    Second5(5, SECOND_OF_MINUTE, 1, HOURS, 3, 1), // 12 * 60 * 3 = 2160
 
-    Minute1(1, MINUTE_OF_HOUR, 4, HOUR_OF_DAY, 7, 1), // 60 * 4 * 7 = 1680
+    Minute1(1, MINUTE_OF_HOUR, 4, HOURS, 7, 1), // 60 * 4 * 7 = 1680
 
-    Minute5(5, MINUTE_OF_HOUR, 6, HOUR_OF_DAY, 20, 1), // 12 * 6 * 20 = 1440
+    Minute5(5, MINUTE_OF_HOUR, 12, HOURS, 12, 1), // 12 * 12 * 12 = 1728
 
-    Minute15(15, MINUTE_OF_HOUR, 1, EPOCH_DAY, 8, 1), // 4 * 24 * 8 = 768
+    Minute15(15, MINUTE_OF_HOUR, 1, DAYS, 12, 1), // 4 * 24 * 12 = 1152
 
-    Minute30(30, MINUTE_OF_HOUR, 1, EPOCH_DAY, 16, 1), // 2 * 24 * 16 = 768
+    Minute30(30, MINUTE_OF_HOUR, 1, DAYS, 24, 1), // 2 * 24 * 24 = 1152
 
-    Hour1(1, HOUR_OF_DAY, 1, EPOCH_DAY, 40, 1), // 24 * 40 = 960
+    Hour1(1, HOUR_OF_DAY, 2, DAYS, 16, 1), // 24 * 2 * 16 = 768
 
-    Hour2(2, HOUR_OF_DAY, 1, EPOCH_DAY, 80, 1, 2), // 12 * 80 =960
+    Hour2(2, HOUR_OF_DAY, 5, DAYS, 10, 1, 2), // 12 * 5 * 10 =600
 
-    Hour4(4, HOUR_OF_DAY, 1, EPOCH_DAY, 160), // 6 * 160 = 960
+    Hour4(4, HOUR_OF_DAY, 10, DAYS, 10), // 6 * 10 * 10 = 600
 
-    Hour6(6, HOUR_OF_DAY, 1, EPOCH_DAY, 240, 1), // 4 * 240 = 960
+    Hour6(6, HOUR_OF_DAY, 15, DAYS, 10, 1), // 4 * 15 * 10 = 600
 
-    Hour12(12, HOUR_OF_DAY, 1, EPOCH_DAY, 480, 1), // 2 * 480 = 960
+    Hour12(12, HOUR_OF_DAY, 30, DAYS, 10, 1), // 2 * 30 * 10 = 600
 
-    Day1(1, EPOCH_DAY, 365, EPOCH_DAY, 3, 1, 2),
+    Day1(1, EPOCH_DAY, 60, DAYS, 10, 1, 2), // 60 * 10 = 600
 
-    Day3(3, EPOCH_DAY, 366 /* 3x122 */, EPOCH_DAY, 6),
+    Day3(3, EPOCH_DAY, 180, DAYS, 10), // 60 * 10 = 600
 
-    Day7(7, EPOCH_DAY, 364 /* 7x52 */, EPOCH_DAY, 12);
+    Day7(7, EPOCH_DAY, 364 /* 7x52 */, DAYS, 10); // 52 * 10 = 520
 
-    /** The duration of this {@link Span}. */
+    /** The actual duration. */
     public final Duration duration;
 
-    /** The duration (seconds). */
+    /** The actual duration (seconds). */
     public final long seconds;
 
     /** The duration (seconds) of this {@link Span}'s segment. */
-    public final long segment;
+    final long segment;
 
     /** The maximum size of this {@link Span}'s segment. */
-    public final int segmentSize;
+    final int segmentSize;
 
     /** The indexes of associated upper tickers. */
     final int[] uppers;
@@ -73,16 +75,20 @@ public enum Span {
     /** The unit name. */
     private final Variable<String> unitName;
 
+    public static void main(String[] args) {
+        System.out.println(Span.Minute1.duration.getSeconds());
+    }
+
     /**
      * @param amount
      * @param unit
      */
-    private Span(long amount, ChronoField unit, int segment, ChronoField segmentUnit, int maximumSegmentSize, int... uppers) {
+    private Span(long amount, ChronoField unit, int segment, ChronoUnit segmentUnit, int maximumSegmentSize, int... uppers) {
         this.amount = amount;
         this.unit = unit;
         this.duration = Duration.of(amount, unit.getBaseUnit());
         this.seconds = duration.getSeconds();
-        this.segment = Duration.of(segment, segmentUnit.getBaseUnit()).toSeconds();
+        this.segment = Duration.of(segment, segmentUnit).toSeconds();
         this.segmentSize = maximumSegmentSize;
         this.uppers = new int[uppers.length];
         this.unitName = unit();
