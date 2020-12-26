441d890f96b86a94297640b63a3ff88eef4c852c
Teletha <Teletha.T@gmail.com>
2020-12-26 09:26:16 +0900

    Revert.

--- src/main/java/cointoss/market/ftx/FTXService.java
+++ src/main/java/cointoss/market/ftx/FTXService.java
@@ -29,7 +29,6 @@ import cointoss.market.Exchange;
 import cointoss.market.TimestampBasedMarketServiceSupporter;
 import cointoss.order.OrderBookPage;
 import cointoss.order.OrderBookPageChanges;
-import cointoss.ticker.data.OpenInterest;
 import cointoss.util.APILimiter;
 import cointoss.util.Chrono;
 import cointoss.util.EfficientWebSocket;
@@ -252,22 +251,17 @@ public class FTXService extends MarketService {
         return change;
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public Signal<OpenInterest> provideOpenInterest(ZonedDateTime startExcluded) {
-        return call("GET", "options/historical_open_interest/" + "BTC" + "?limit=200&start_time=1559881511").map(e -> {
-            System.out.println(e);
-            return null;
-        });
-    }
-
-    public static void main(String[] args) {
-        FTX.BTC_PERP.provideOpenInterest(Chrono.utc(2020, 12, 20)).waitForTerminate().to(e -> {
-            System.out.println(e);
-        });
-    }
+    // /**
+    // * {@inheritDoc}
+    // */
+    // @Override
+    // public Signal<OpenInterest> provideOpenInterest(ZonedDateTime startExcluded) {
+    // return call("GET", "options/historical_open_interest/" + "BTC" +
+    // "?limit=200&start_time=1559881511").map(e -> {
+    // System.out.println(e);
+    // return null;
+    // });
+    // }
 
     /**
      * Call rest API.
