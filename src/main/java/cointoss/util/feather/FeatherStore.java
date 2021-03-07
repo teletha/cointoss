fa4952c2c0ca8eae124971a6063c5ff2f93f5fc3
Teletha <Teletha.T@gmail.com>
2021-03-07 10:00:44 +0900

    Progress...

--- src/main/java/cointoss/util/feather/FeatherStore.java
+++ src/main/java/cointoss/util/feather/FeatherStore.java
@@ -292,8 +292,8 @@ public final class FeatherStore<E extends TemporalData> implements Disposable {
 
         if (segment == null) {
             segment = new OnHeap(model, index[0], itemSize);
-            indexed.put(index[0], segment);
             tryEvict(index[0]);
+            indexed.put(index[0], segment);
         }
 
         if (accumulator == null) {
@@ -363,10 +363,20 @@ public final class FeatherStore<E extends TemporalData> implements Disposable {
         return segment.get((int) index[1]);
     }
 
+    /**
+     * Get epoch time of the earliest item.
+     * 
+     * @return
+     */
     public long firstTime() {
         return first;
     }
 
+    /**
+     * Get epoch time of the latest item.
+     * 
+     * @return
+     */
     public long lastTime() {
         return last;
     }
@@ -822,8 +832,8 @@ public final class FeatherStore<E extends TemporalData> implements Disposable {
             if (1 <= result[0]) {
                 segment.min = result[1];
                 segment.max = result[2];
-                indexed.put(startTime, segment);
                 tryEvict(startTime);
+                indexed.put(startTime, segment);
 
                 return segment;
             }
@@ -878,7 +888,6 @@ public final class FeatherStore<E extends TemporalData> implements Disposable {
             }
             segment.clear();
 
-            System.out.println(evictableTime + "  " + first + "  " + indexed.keySet());
             if (evictableTime <= first) {
                 OnHeap<E> heap = indexed.firstValue();
                 first = heap == null ? Long.MAX_VALUE : heap.first().seconds();
