18e6bd869c21f266fc346b709b8e4859b38c1218
Teletha <Teletha.T@gmail.com>
2021-03-28 09:18:38 +0900

    Progress...

--- src/main/java/cointoss/market/binance/Binance.java
+++ src/main/java/cointoss/market/binance/Binance.java
@@ -85,11 +85,12 @@ public final class Binance extends MarketServiceProvider {
             .base(Currency.USDT.minimumSize(0.000001))
             .acquirableExecutionSize(AcquirableSize));
 
-    public static final MarketService FUTURE_BTCUSD_210326 = new BinanceService("BTCUSD_210326", MarketSetting.with.derivative()
-            .target(Currency.BTC.minimumSize(0.00001))
-            .base(Currency.USD.minimumSize(0.1))
-            .priceRangeModifier(500)
-            .acquirableExecutionSize(AcquirableSize));
+    // public static final MarketService FUTURE_BTCUSD_210326 = new BinanceService("BTCUSD_210326",
+    // MarketSetting.with.derivative()
+    // .target(Currency.BTC.minimumSize(0.00001))
+    // .base(Currency.USD.minimumSize(0.1))
+    // .priceRangeModifier(500)
+    // .acquirableExecutionSize(AcquirableSize));
 
     public static final MarketService FUTURE_BTCUSD_210625 = new BinanceService("BTCUSD_210625", MarketSetting.with.derivative()
             .target(Currency.BTC.minimumSize(0.00001))
