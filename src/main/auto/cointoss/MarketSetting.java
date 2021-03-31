ccc02bab9543adbda760c52150d4bb4e87f5575c
Teletha <Teletha.T@gmail.com>
2021-03-31 16:58:46 +0900

    progress...

--- src/main/auto/cointoss/MarketSetting.java
+++ src/main/auto/cointoss/MarketSetting.java
@@ -595,6 +595,17 @@ public class MarketSetting implements MarketSettingModel {
          * 
          * @return The next assignable model.
          */
+        public ÅssignableTarget<ÅssignableBase<Self>> spot() {
+            Åssignable o = new Åssignable();
+            o.spot();
+            return o;
+        }
+
+        /**
+         * Create new {@link MarketSetting} with the specified type property.
+         * 
+         * @return The next assignable model.
+         */
         public ÅssignableTarget<ÅssignableBase<Self>> derivative() {
             Åssignable o = new Åssignable();
             o.derivative();
@@ -609,17 +620,6 @@ public class MarketSetting implements MarketSettingModel {
         public ÅssignableTarget<ÅssignableBase<Self>> future() {
             Åssignable o = new Åssignable();
             o.future();
-            return o;
-        }
-
-        /**
-         * Create new {@link MarketSetting} with the specified type property.
-         * 
-         * @return The next assignable model.
-         */
-        public ÅssignableTarget<ÅssignableBase<Self>> spot() {
-            Åssignable o = new Åssignable();
-            o.spot();
             return o;
         }
     }
@@ -645,6 +645,15 @@ public class MarketSetting implements MarketSettingModel {
          * 
          * @return The next assignable model.
          */
+        default Next spot() {
+            return type(MarketType.SPOT);
+        }
+
+        /**
+         * Assign type property.
+         * 
+         * @return The next assignable model.
+         */
         default Next derivative() {
             return type(MarketType.DERIVATIVE);
         }
@@ -656,15 +665,6 @@ public class MarketSetting implements MarketSettingModel {
          */
         default Next future() {
             return type(MarketType.FUTURE);
-        }
-
-        /**
-         * Assign type property.
-         * 
-         * @return The next assignable model.
-         */
-        default Next spot() {
-            return type(MarketType.SPOT);
         }
     }
 
