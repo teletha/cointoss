ec3aade708caa0c5470b4859040e96e8a0710b0b
Teletha <Teletha.T@gmail.com>
2022-02-03 10:08:22 +0900

    progress

---
+++ src/main/auto/cointoss/ticker/data/OpenInterest.java
@@ -0,0 +1,300 @@
+package cointoss.ticker.data;
+
+import cointoss.ticker.data.OpenInterest;
+import java.lang.Override;
+import java.lang.StringBuilder;
+import java.lang.Throwable;
+import java.lang.UnsupportedOperationException;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.time.ZonedDateTime;
+import java.util.Objects;
+
+/**
+ * Generated model for {@link OpenInterestModel}.
+ * 
+ * @see <a href="https://github.com/teletha/icymanipulator">Icy Manipulator (Code Generator)</a>
+ */
+public class OpenInterest extends OpenInterestModel {
+
+    /**
+     * Deceive complier that the specified checked exception is unchecked exception.
+     *
+     * @param <T> A dummy type for {@link RuntimeException}.
+     * @param throwable Any error.
+     * @return A runtime error.
+     * @throws T Dummy error to deceive compiler.
+     */
+    private static final <T extends Throwable> T quiet(Throwable throwable) throws T {
+        throw (T) throwable;
+    }
+
+    /**
+     * Create special method invoker.
+     *
+     * @param name A target method name.
+     * @param parameterTypes A list of method parameter types.
+     * @return A special method invoker.
+     */
+    private static final MethodHandle invoker(String name, Class... parameterTypes)  {
+        try {
+            Method method = cointoss.ticker.data.OpenInterestModel.class.getDeclaredMethod(name, parameterTypes);
+            method.setAccessible(true);
+            return MethodHandles.lookup().unreflect(method);
+        } catch (Throwable e) {
+            throw quiet(e);
+        }
+    }
+
+    /** The overload or intercept method invoker. */
+    private static final MethodHandle date$1093866057= invoker("date", long.class);
+
+    /**
+     * Create special property updater.
+     *
+     * @param name A target property name.
+     * @return A special property updater.
+     */
+    private static final MethodHandle updater(String name)  {
+        try {
+            Field field = OpenInterest.class.getDeclaredField(name);
+            field.setAccessible(true);
+            return MethodHandles.lookup().unreflectSetter(field);
+        } catch (Throwable e) {
+            throw quiet(e);
+        }
+    }
+
+    /** The final property updater. */
+    private static final MethodHandle dateUpdater = updater("date");
+
+    /** The final property updater. */
+    private static final MethodHandle sizeUpdater = updater("size");
+
+    /** The exposed property. */
+    public final ZonedDateTime date;
+
+    /** The exposed property. */
+    public final float size;
+
+    /**
+     * HIDE CONSTRUCTOR
+     */
+    protected OpenInterest() {
+        this.date = null;
+        this.size = 0;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final ZonedDateTime date() {
+        return this.date;
+    }
+
+    /**
+     * Provide classic getter API.
+     *
+     * @return A value of date property.
+     */
+    @SuppressWarnings("unused")
+    private final ZonedDateTime getDate() {
+        return this.date;
+    }
+
+    /**
+     * Provide classic setter API.
+     *
+     * @paran value A new value of date property to assign.
+     */
+    private final void setDate(ZonedDateTime value) {
+        if (value == null) {
+            throw new IllegalArgumentException("The date property requires non-null value.");
+        }
+        try {
+            dateUpdater.invoke(this, value);
+        } catch (UnsupportedOperationException e) {
+        } catch (Throwable e) {
+            throw quiet(e);
+        }
+    }
+
+    /**
+     * Return the size property.
+     *
+     * @return A value of size property.
+     */
+    @Override
+    public final float size() {
+        return this.size;
+    }
+
+    /**
+     * Provide classic getter API.
+     *
+     * @return A value of size property.
+     */
+    @SuppressWarnings("unused")
+    private final float getSize() {
+        return this.size;
+    }
+
+    /**
+     * Provide classic setter API.
+     *
+     * @paran value A new value of size property to assign.
+     */
+    private final void setSize(float value) {
+        try {
+            sizeUpdater.invoke(this, value);
+        } catch (UnsupportedOperationException e) {
+        } catch (Throwable e) {
+            throw quiet(e);
+        }
+    }
+
+    /**
+     * Show all property values.
+     *
+     * @return All property values.
+     */
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder("OpenInterest [");
+        builder.append("date=").append(date).append(", ");
+        builder.append("size=").append(size).append("]");
+        return builder.toString();
+    }
+
+    /**
+     * Generates a hash code for a sequence of property values. The hash code is generated as if all the property values were placed into an array, and that array were hashed by calling Arrays.hashCode(Object[]). 
+     *
+     * @return A hash value of the sequence of property values.
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(date, size);
+    }
+
+    /**
+     * Returns true if the all properties are equal to each other and false otherwise. Consequently, if both properties are null, true is returned and if exactly one property is null, false is returned. Otherwise, equality is determined by using the equals method of the base model. 
+     *
+     * @return true if the all properties are equal to each other and false otherwise.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof OpenInterest == false) {
+            return false;
+        }
+
+        OpenInterest other = (OpenInterest) o;
+        if (!Objects.equals(date, other.date)) return false;
+        if (size != other.size) return false;
+        return true;
+    }
+
+    /** The singleton builder. */
+    public static final  Ìnstantiator<?> with = new Ìnstantiator();
+
+    /**
+     * Namespace for {@link OpenInterest}  builder methods.
+     */
+    public static class Ìnstantiator<Self extends OpenInterest & ÅssignableÅrbitrary<Self>> {
+
+        /**
+         * Create new {@link OpenInterest} with the specified date property.
+         * 
+         * @return The next assignable model.
+         */
+        public ÅssignableSize<Self> date(ZonedDateTime date) {
+            Åssignable o = new Åssignable();
+            o.date(date);
+            return o;
+        }
+
+        /**
+         * Create new {@link OpenInterest} with the specified date property.
+         * 
+         * @return The next assignable model.
+         */
+        public ÅssignableSize<Self> date(long time) {
+            Åssignable o = new Åssignable();
+            o.date(time);
+            return o;
+        }
+    }
+
+    /**
+     * Property assignment API.
+     */
+    public static interface ÅssignableDate<Next> {
+
+        /**
+         * Assign date property.
+         * 
+         * @param value A new value to assign.
+         * @return The next assignable model.
+         */
+        default Next date(ZonedDateTime value) {
+            ((OpenInterest) this).setDate(value);
+            return (Next) this;
+        }
+
+        /**
+         * Assign date property.
+         * 
+         * @return The next assignable model.
+         */
+        default Next date(long time) {
+            try {
+                return date((ZonedDateTime) date$1093866057.invoke(this, time));
+            } catch (Throwable e) {
+                throw quiet(e);
+            }
+        }
+    }
+
+    /**
+     * Property assignment API.
+     */
+    public static interface ÅssignableSize<Next> {
+
+        /**
+         * Assign size property.
+         * 
+         * @param value A new value to assign.
+         * @return The next assignable model.
+         */
+        default Next size(float value) {
+            ((OpenInterest) this).setSize(value);
+            return (Next) this;
+        }
+    }
+
+    /**
+     * Property assignment API.
+     */
+    public static interface ÅssignableÅrbitrary<Next extends OpenInterest> {
+    }
+
+    /**
+     * Internal aggregated API.
+     */
+    protected static interface ÅssignableAll extends ÅssignableDate, ÅssignableSize {
+    }
+
+    /**
+     * Mutable Model.
+     */
+    private static final class Åssignable extends OpenInterest implements ÅssignableAll, ÅssignableÅrbitrary {
+    }
+
+    /**
+     * The identifier for properties.
+     */
+    static final class My {
+        static final String Date = "date";
+        static final String Size = "size";
+    }
+}
