7f3e3e1b7eb44c7305efc5481b86797af1f0106c
Teletha <Teletha.T@gmail.com>
2023-08-03 23:59:02 +0900

    update license

--- src/main/java/cointoss/verify/VerifiableMarket.java
+++ src/main/java/cointoss/verify/VerifiableMarket.java
@@ -1,6 +1,6 @@
 
 /*
- * Copyright (C) 2021 cointoss Development Team
+ * Copyright (C) 2023 The COINTOSS Development Team
  *
  * Licensed under the MIT License (the "License");
  * you may not use this file except in compliance with the License.
