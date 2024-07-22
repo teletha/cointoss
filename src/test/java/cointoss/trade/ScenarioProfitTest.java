b7b49e11f23f40144ff8aafc34409ef9ca16995e
Teletha <Teletha.T@gmail.com>
2024-07-22 14:17:12 +0900

    test more

--- src/test/java/cointoss/trade/ScenarioProfitTest.java
+++ src/test/java/cointoss/trade/ScenarioProfitTest.java
@@ -38,6 +38,30 @@ class ScenarioProfitTest extends TraderTestSupport {
     }
 
     @TradeTest
+    void realizedProfitExitTwice(SidePart side, SizePart size, PricePart price) {
+        Scenario s = entry(side, size, o -> o.make(price.entry));
+        assert s.realizedProfit.is(0);
+
+        executeEntryHalf();
+        assert s.realizedProfit.is(0);
+
+        executeEntryAll();
+        assert s.realizedProfit.is(0);
+
+        exit(0.5, o -> o.make(price.exit));
+        assert s.realizedProfit.is(0);
+
+        executeExitAll();
+        assert s.realizedProfit.is(size.half * price.diff * side.sign);
+
+        exit(0.5, o -> o.make(price.exit));
+        assert s.realizedProfit.is(size.half * price.diff * side.sign);
+
+        executeExitAll();
+        assert s.realizedProfit.is(size.num * price.diff * side.sign);
+    }
+
+    @TradeTest
     void unrealizedProfit(SidePart side, SizePart size, PricePart price) {
         Scenario s = entry(side, size, o -> o.make(price.entry));
         assert s.unrealizedProfit(price.middleN).is(0);
@@ -132,6 +156,44 @@ class ScenarioProfitTest extends TraderTestSupport {
         assert s.profit(price.lossN).is(size.num * -price.diffHalf * side.sign);
 
         executeExitHalf();
+        assert s.profit(price.middleN).is(size.half * price.diffHalf * side.sign + size.half * price.diff * side.sign);
+        assert s.profit(price.entryN).is(size.half * 0 * side.sign + size.half * price.diff * side.sign);
+        assert s.profit(price.lossN).is(size.half * -price.diffHalf * side.sign + size.half * price.diff * side.sign);
+
+        executeExitAll();
+        assert s.profit(price.middleN).is(size.num * price.diff * side.sign);
+        assert s.profit(price.entryN).is(size.num * price.diff * side.sign);
+        assert s.profit(price.lossN).is(size.num * price.diff * side.sign);
+    }
+
+    @TradeTest
+    void profitExitTwice(SidePart side, SizePart size, PricePart price) {
+        Scenario s = entry(side, size, o -> o.make(price.entry));
+        assert s.profit(price.middleN).is(0);
+        assert s.profit(price.entryN).is(0);
+        assert s.profit(price.lossN).is(0);
+
+        executeEntryHalf();
+        assert s.profit(price.middleN).is(size.half * price.diffHalf * side.sign);
+        assert s.profit(price.entryN).is(size.half * 0 * side.sign);
+        assert s.profit(price.lossN).is(size.half * -price.diffHalf * side.sign);
+
+        executeEntryAll();
+        assert s.profit(price.middleN).is(size.num * price.diffHalf * side.sign);
+        assert s.profit(price.entryN).is(size.num * 0 * side.sign);
+        assert s.profit(price.lossN).is(size.num * -price.diffHalf * side.sign);
+
+        exit(0.5, o -> o.make(price.exit));
+        assert s.profit(price.middleN).is(size.num * price.diffHalf * side.sign);
+        assert s.profit(price.entryN).is(size.num * 0 * side.sign);
+        assert s.profit(price.lossN).is(size.num * -price.diffHalf * side.sign);
+
+        executeExitAll();
+        assert s.profit(price.middleN).is(size.half * price.diffHalf * side.sign + size.half * price.diff * side.sign);
+        assert s.profit(price.entryN).is(size.half * 0 * side.sign + size.half * price.diff * side.sign);
+        assert s.profit(price.lossN).is(size.half * -price.diffHalf * side.sign + size.half * price.diff * side.sign);
+
+        exit(0.5, o -> o.make(price.exit));
         assert s.profit(price.middleN).is(size.half * price.diffHalf * side.sign + size.half * price.diff * side.sign);
         assert s.profit(price.entryN).is(size.half * 0 * side.sign + size.half * price.diff * side.sign);
         assert s.profit(price.lossN).is(size.half * -price.diffHalf * side.sign + size.half * price.diff * side.sign);
