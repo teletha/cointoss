c48d999aa4bed33ad6e0fe84f3ebcf456bdc0931
Teletha <Teletha.T@gmail.com>
2024-09-25 05:19:33 +0900

    fix compile error

--- src/test/java/cointoss/ticker/TickerManagerBuildTest.java
+++ src/test/java/cointoss/ticker/TickerManagerBuildTest.java
@@ -18,6 +18,7 @@ import org.junit.jupiter.api.Test;
 
 import cointoss.TestableMarketService;
 import cointoss.util.Chrono;
+import cointoss.util.DateRange;
 import cointoss.util.feather.FeatherStore;
 
 public class TickerManagerBuildTest {
@@ -44,12 +45,13 @@ public class TickerManagerBuildTest {
     void buildByRange() {
         ZonedDateTime start = Chrono.utc(2020, 1, 1);
         ZonedDateTime end = Chrono.utc(2020, 1, 2, 23, 59, 0, 0);
+        DateRange range = DateRange.between(start, end);
 
         TestableMarketService service = new TestableMarketService();
         service.log.generateFastLog(start, end, Span.Minute1);
 
         TickerManager manager = new TickerManager(service);
-        manager.build(start, end, false).to();
+        manager.build(range, false).to();
         assert manager.on(Span.Day).ticks.query(start, end).toList().size() == 2;
         assert manager.on(Span.Hour4).ticks.query(start, end).toList().size() == 12;
         assert manager.on(Span.Hour1).ticks.query(start, end).toList().size() == 48;
@@ -62,6 +64,7 @@ public class TickerManagerBuildTest {
     void rebuildByRange() {
         ZonedDateTime start = Chrono.utc(2020, 1, 1);
         ZonedDateTime end = Chrono.utc(2020, 1, 2, 23, 59, 0, 0);
+        DateRange range = DateRange.between(start, end);
 
         // generate fast log
         TestableMarketService service = new TestableMarketService();
@@ -71,7 +74,7 @@ public class TickerManagerBuildTest {
         FeatherStore<Tick> ticks = manager.on(Span.Minute15).ticks;
 
         // build ticker data from fast log
-        manager.build(start, end, false).to();
+        manager.build(range, false).to();
         List<Tick> oldTicks = ticks.query(start, end).toList();
         List<Tick> newTicks = ticks.query(start, end).toList();
         assert same(oldTicks, newTicks);
@@ -83,20 +86,20 @@ public class TickerManagerBuildTest {
 
         // clear memory cache and rebuild ticker data from same fast log
         ticks.clear();
-        manager.build(start, end, false).to();
+        manager.build(range, false).to();
         newTicks = ticks.query(start, end).toList();
         assert same(oldTicks, newTicks);
 
         // clear memory cache and rebuild ticker data from same fast log
         ticks.clear();
-        manager.build(start, end, true).to();
+        manager.build(range, true).to();
         newTicks = ticks.query(start, end).toList();
         assert same(oldTicks, newTicks);
 
         // clear memory cache and rebuild ticker data from new generated fast log
         ticks.clear();
         service.log.generateFastLog(start, end, Span.Hour1);
-        manager.build(start, end, true).to();
+        manager.build(range, true).to();
         newTicks = ticks.query(start, end).toList();
         assert different(oldTicks, newTicks);
     }
@@ -116,19 +119,19 @@ public class TickerManagerBuildTest {
         FeatherStore<Tick> ticks = manager.on(Span.Day).ticks;
 
         // build inside only
-        manager.build(insideStart, insideEnd, false).to();
+        manager.build(DateRange.between(insideStart, insideEnd), false).to();
         assert ticks.first().date().equals(insideStart);
         assert ticks.last().date().equals(insideEnd);
         List<Tick> inside = ticks.query(insideStart, insideEnd).toList();
 
         // build with previous data
-        manager.build(start, insideEnd, false).to();
+        manager.build(DateRange.between(start, insideEnd), false).to();
         assert ticks.first().date().equals(start);
         assert ticks.last().date().equals(insideEnd);
         assert same(inside, ticks.query(insideStart, insideEnd).toList());
 
         // build with next data
-        manager.build(insideStart, end, false).to();
+        manager.build(DateRange.between(insideStart, end), false).to();
         assert ticks.first().date().equals(start);
         assert ticks.last().date().equals(end);
         assert same(inside, ticks.query(insideStart, insideEnd).toList());
@@ -152,13 +155,13 @@ public class TickerManagerBuildTest {
         assert ticks.query(start, end).toList().size() == 0;
 
         // build inside only
-        manager.build(inside1Start, inside1End, false).to();
+        manager.build(DateRange.between(inside1Start, inside1End), false).to();
         assert ticks.query(start, end).toList().size() == 4;
-        manager.build(inside2Start, inside2End, false).to();
+        manager.build(DateRange.between(inside2Start, inside2End), false).to();
         assert ticks.query(start, end).toList().size() == 10;
 
         // build with full data
-        manager.build(start, end, false).to();
+        manager.build(DateRange.between(start, end), false).to();
         assert ticks.first().date().equals(start);
         assert ticks.last().date().equals(end);
         assert ticks.query(start, end).toList().size() == 15;
